7f7e5f25b66cf1a0885697ce85ac0c7e5367c83a
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-11-22 19:11:49 +0100

    update

--- src/main/java/com/formation/velo/service/impl/ParkingServiceImpl.java
+++ src/main/java/com/formation/velo/service/impl/ParkingServiceImpl.java
@@ -67,7 +67,7 @@ public class ParkingServiceImpl implements ParkingService{
             log.info(openDataVeloNantes.toString());
 
             Arrays.stream(openDataVeloNantes.getRecords()).forEach(record -> {
-                Optional<Parking> parkingToUpdate = findByRecordId(record.getRecordId());
+                Optional<Parking> parkingToUpdate = findByRecordId(record.getField().getGrpIdentifiant());
 
                 if(parkingToUpdate.isPresent()){
                     //On update le parking
